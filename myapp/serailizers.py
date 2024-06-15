6b210742d468e9f5b7ec4a3b0e6309c78608e54f
Imeanabhi <abhi.nagabothu@gmail.com>
2024-06-16 01:08:39 +0530

    added serializer

---
+++ myapp/serailizers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from myapp.models import Image,UserInterface
+    
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Image
+        fields="__all__"
+
+class UserInterfaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=UserInterface
+        fields="__all__"
